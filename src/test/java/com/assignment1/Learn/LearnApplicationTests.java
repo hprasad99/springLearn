5510e72190a3bde2349e341f31df5e17ef420dda
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2022-01-04 23:22:13 +0530

    initialized structure

---
+++ src/test/java/com/assignment1/Learn/LearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment1.Learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
